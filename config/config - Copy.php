fde34c8636e7809f53289b940d935fd7b90ed1a0
jelenazj <44589316+jelenazj@users.noreply.github.com>
2019-06-10 22:26:05 +0200

    Add files via upload

---
+++ config/config - Copy.php
@@ -0,0 +1,6 @@
+<?php
+	const SERVER = "localhost";
+	const USER = "id9207869_admin";
+	const PASS = "adminadmin";
+	const DB = "id9207869_bootstorm";
+?>
---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+	const SERVER = "localhost";
+	const USER = "root";
+	const PASS = "";
+	const DB = "bootstorm";
+?>
